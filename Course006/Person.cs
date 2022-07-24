1e52dbee8f17ad4f5d3ffa310dea369ff3aa73a4
shijiangshan <shijiangshan@bjknrt.com>
2022-07-24 18:53:28 +0800

    第六天课程完成，不容易呀，是在一天内完成的

---
+++ Course006/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course006
+{
+    public class Person
+    {
+        public int Gender { get; set; }
+
+        public string Name { get; set; }
+    }
+}
