da56b7f98ad60a68c5a67dd62031b98c2811d641
shijiangshan <shijiangshan@bjknrt.com>
2022-09-20 11:02:40 +0800

    保存

---
+++ Course021/Base/CommandBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Course021.Base
+{
+    public class CommandBase : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            DoExecute?.Invoke(parameter);
+        }
+
+        public Action<object> DoExecute { get; set; }
+
+        public CommandBase(Action<object> doExecute)
+        {
+            DoExecute = doExecute;
+        }
+    }
+}
---
+++ Course021/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Course021.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Course021
+{
+    public class MainWindowViewModel
+    {
+
+        public ICommand MouseCommand { get; set; }
+        public ICommand KeyCommand { get; set; }
+
+        public ICommand ComboSelectionChangedCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            MouseCommand = new CommandBase(Mouse);
+            KeyCommand = new CommandBase(Key);
+            ComboSelectionChangedCommand = new CommandBase(ComboSelectionChanged);
+        }
+
+        private void Key(object obj)
+        {
+
+        }
+
+        private void Mouse(object obj)
+        {
+
+        }
+
+        private void ComboSelectionChanged(object obj)
+        {
+
+        }
+
+        public void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
